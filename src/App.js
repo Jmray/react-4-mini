bdd7ff44c7cdeb604b2d84ffad1ee33657778a1c
Justus Ray <justusmray@gmail.com>
2019-02-14 20:11:18 -0700

    this is the finished mini project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import React, { Component } from 'react';
+import { HashRouter, Route, Switch, Link, Redirect } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Route1 from './components/Route1';
+import Route2 from './components/Route2';
+import Route3 from './components/Route3';
+import NotFound from './components/NotFound';
+
 
 class App extends Component {
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <HashRouter>
+        <div className="App">
+          <header className="App-header">
+          <nav>
+            <Link to="/1">To Route 1</Link>
+            <Link to="/2">To Route 2</Link>
+            <Link to="/3">To Route 3</Link>
+          </nav>
+            <img src={logo} className="App-logo" alt="logo" />
+            <Switch>
+              <Route path="/1" component={Route1} />
+              <Route path="/2" component={Route2} />
+              <Route path="/3" component={Route3} />
+              <Route path="/not-found" component={NotFound} />
+              <Redirect to="/not-found"/>
+               
+            </Switch>
+          </header>
+        </div>
+      </HashRouter>
     );
   }
+
 }
 
 export default App;
---
+++ src/components/Route1.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    Link,
+    Route,
+    Switch,
+} from 'react-router-dom';
+
+
+
+
+function Route1(props) {
+    console.log(props);
+
+
+    return (
+        <h2>Route 1</h2>
+    );
+}
+
+export default Route1;
